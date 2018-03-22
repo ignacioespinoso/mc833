2f96845620ee765ef837f72b77691c1e540530e3
Ignacio Espinoso <ignacioespinoso@gmail.com>
2018-03-22 11:20:58 -0300

    First commit

---
+++ proj1/server.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+int getaddrinfo(const char *node, const char *service,
+const struct addrinfo *hints, struct addrinfo **res);
+int main() {
+
+  //Creates socket
+  //Creates listen port
+  //Binds port to socket and listens
+  //Do stuff
+  return 0;
+}
